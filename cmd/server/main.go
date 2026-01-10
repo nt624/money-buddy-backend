64c1ee7d353ff320f04666ee045eec121ba1eacc
nt624 <taku8606.uec@gmail.com>
2026-01-10 11:50:29 +0900

    #32 CORSにDELETEを追加する

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	r.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if c.Request.Method == "OPTIONS" {
